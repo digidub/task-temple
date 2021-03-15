974131ec6d2711bb338ab22d78662c2330675cb4
Sholto <madein.mania@gmail.com>
2021-03-15 11:21:56 +0000

    project load and save functions

--- src/appcontrol.js
+++ src/appcontrol.js
@@ -1,7 +1,42 @@
-const appcontrol = (() => {
+import { appData } from "./appdata";
+import { lsControl } from "./localstorage";
+
+const appControl = (() => {
+
+const genDefaultProject = () => {
+    let defaultProject = Project("Default Project")
+}
+
+const genDefaultTask = () => {
+    let defaultTask = Task("Create your first project (or task!)", "tasktemple lets you organise your to do list by groups - or projects. Set due date, priority, and add notes")
+}
+
+const storageCheck = (obj) => {
+    lsControl.storageCheck(obj)
+}
+
+const restore = (obj) => {
+    appData.projects.push(obj)
+}
+
+const projectLoader = () => {
+    let fetchedProjects = storageCheck('projects');
+    restore(fetchedProjects)
+}
+
+const projectSaver = (obj) => {
+    lsControl.saveStorage(obj)
+}
 
 
-
-
+return {
+    genDefaultProject,
+    genDefaultTask,
+    projectLoader,
+    restore,
+    storageCheck,
+}
 
 })();
+
+export { appControl }
