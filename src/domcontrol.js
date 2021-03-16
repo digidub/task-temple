ae0b905579077ed82cb58ee7d44716b6b258a3a6
Sholto <madein.mania@gmail.com>
2021-03-16 09:41:36 +0000

    initial dom controls - display tasks and projects

--- src/domcontrol.js
+++ src/domcontrol.js
@@ -1,5 +1,55 @@
+import { appData } from "./appdata";
+
 const DOMcontrol = (() => {
 
+    const projectView = document.querySelector(".project-view-list")
+    const taskView = document.querySelector(".task-view-list")
+
+
+    const createElement = (el) => {
+        return document.createElement(el)
+    }
+
+    const styleElement = (el, style) => {
+        el.classList.add(`${style}`)
+    }
+
+    const textElement = (el, name) => {        
+        el.innerText = `${name}`
+    }
+
+    const setupDiv = (name) => {        
+        let div = createElement("div")
+        let p = createElement("p")        
+        styleElement(div, "project-name")
+        textElement(p, name)
+        div.appendChild(p)
+        
+        return div
+    }
+
+    const displayProjects = () => {
+        appData.projects.forEach(project => {            
+            let div = setupDiv(project.name)            
+            projectView.appendChild(div);
+        })
+    }
+
+    const displayTasks = () => {
+        appData.projects.forEach(project => {
+            project.tasks.forEach(task => {
+                let div = setupDiv(task.name)
+                taskView.appendChild(div);
+            })            
+        })
+    }
+
+    return {
+        displayProjects,
+        displayTasks
+    }
 
 
 })();
+
+export { DOMcontrol }
