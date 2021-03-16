f25eb89f5b80418e440dad5437e0eb6b020174d4
Sholto <madein.mania@gmail.com>
2021-03-16 15:19:48 +0000

    object to DOM module

---
+++ src/objectdom.js
@@ -0,0 +1,34 @@
+const ObjectToDOM = (() => {
+
+    const gen = obj => {
+
+        let domEl = document.createElement(obj.tag)
+
+        if (obj.content) domEl.innerText = obj.content
+        if (obj.classes) for (let cssClass of obj.classes) {
+            domEl.classList.add(cssClass)
+        }
+        if (obj.id) domEl.id = obj.id
+        if (obj.for) domEl.htmlFor = obj.for
+        if (obj.placeholder) domEl.placeholder = obj.placeholder
+        if (obj.tag) domEl.tag = obj.tag
+        if (obj.type) domEl.type = obj.type
+        if (obj.name) domEl.name = obj.name
+        if (obj.value) domEl.value = obj.value
+        if (obj.rows) domEl.rows = obj.rows
+        if (obj.cols) domEl.cols = obj.cols
+        if (obj.selected) domEl.selected = "true"
+        if (obj.required) domEl.required = obj.required
+        if (obj.children) for (let child of obj.children) {
+            DOMgen(child)
+            domEl.append(DOMgen(child))
+        }
+        return domEl
+    }
+
+    return {
+        gen
+    }
+
+})();
+export { ObjectToDOM }
