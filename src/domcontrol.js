229230e891a53837b8e262094c1293fa79f4d85c
Sholto <madein.mania@gmail.com>
2021-03-16 15:27:55 +0000

    click listener and logic for show/hide add project form

--- src/domcontrol.js
+++ src/domcontrol.js
@@ -1,36 +1,42 @@
 import { appData } from "./appdata";
+import { Template } from "./formtemplates";
+import { ObjectToDOM } from "./objectdom";
 
 const DOMcontrol = (() => {
 
     const projectView = document.querySelector(".project-view-list")
     const taskView = document.querySelector(".task-view-list")
-
+    const newProject = document.querySelector(".add-project")
+    const newTask = document.querySelector(".add-task")
 
     const createElement = (el) => {
         return document.createElement(el)
     }
 
-    const styleElement = (el, style) => {
+    const classElement = (el, style) => {
         el.classList.add(`${style}`)
     }
 
-    const textElement = (el, name) => {        
+    const idElement = (el, id) => {
+        el.id = id
+    }
+
+    const textElement = (el, name) => {
         el.innerText = `${name}`
     }
 
-    const setupDiv = (name) => {        
+    const setupDiv = (name, style) => {
         let div = createElement("div")
-        let p = createElement("p")        
-        styleElement(div, "project-name")
+        let p = createElement("p")
+        classElement(div, style)
         textElement(p, name)
         div.appendChild(p)
-        
         return div
     }
 
     const displayProjects = () => {
-        appData.projects.forEach(project => {            
-            let div = setupDiv(project.name)            
+        appData.projects.forEach(project => {
+            let div = setupDiv(project.name, "project-name")
             projectView.appendChild(div);
         })
     }
@@ -38,13 +44,26 @@ const DOMcontrol = (() => {
     const displayTasks = () => {
         appData.projects.forEach(project => {
             project.tasks.forEach(task => {
-                let div = setupDiv(task.name)
+                let div = setupDiv(task.name, "task-name")
                 taskView.appendChild(div);
-            })            
+            })
         })
-    }
+    } 
+
+    function genForm(template) {
+        let form = ObjectToDOM.gen(Template[`${template}`]);
+        projectView.prepend(form); //perhaps revisit this and have it placed in a DIV ABOVE the list of projects..
+    }       
+
+    newProject.addEventListener('click', () => {
+        if (!document.querySelector(".add-project-form")) {
+            genForm("addProject")
+        }
+        else projectView.removeChild(document.querySelector(".add-project-form"))
+    })    
 
     return {
+        newProject,
         displayProjects,
         displayTasks
     }
