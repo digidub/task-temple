7152f6c80e7db9a428167cfb20b8a7b3bf4a8497
Sholto <madein.mania@gmail.com>
2021-03-18 11:35:23 +0000

    set and get active project object

--- src/appdata.js
+++ src/appdata.js
@@ -1,5 +1,21 @@
-const appData = () => {
+const appData = (() => {
     let projects = []
-}
+
+    const activeProject = {
+        id: "",
+    }
+
+    const setActiveProject = (id) => {
+        activeProject.id = id
+    }
+
+    const getActiveProject = () => activeProject.id
+
+    return {        
+        projects,
+        setActiveProject,
+        getActiveProject,
+    }
+})();
 
 export { appData }
