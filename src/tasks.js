1ef9fead2b2ce0dcf061bbaf4ab1f4a57867f9e4
Sholto <madein.mania@gmail.com>
2021-03-18 10:14:51 +0000

    added edit and get functions

--- src/tasks.js
+++ src/tasks.js
@@ -7,14 +7,63 @@ const Task = (taskName, desc, dueDate = null, pri = "normal") => {
     const completed = 0 //0 == uncompleted, 1 == completed
     const priority = pri
 
+    function editName(newName) {
+        return name = newName
+    }
+
+    function editNotes(newNotes) {
+        return notes = newNotes
+    }
+
+    function editDue(newDue) {
+        return due = newDue
+    }
+
+    function editPriority(newPri) {
+        return priority = newPri
+    }
+
+    function getName() {
+        return name;
+    }
+
+    function getDue() {
+        return due;
+    }
+
+    function getPriority() {
+        return priority;
+    }
+
+    function getID() {
+        return id;
+    }
+    
+    /* IN ORDER TO HAVE ID FOR TASKS - will need to have logic that passes through the overriding project that creates the task, in order to measure its length.
+    Maybe this could be done by having a property in the task object with the project's name in it. 
+    const id = genID()
+
+    function genID() {
+        if (appData.projects.length < 1) {
+            let id = 1;
+            return id;
+        } else {
+            let arr = appData.projects;
+            let maxID = Math.max(...array.map(arr => arr.id));
+            let id = maxID + 1;
+            return id;
+        }
+    }*/
 
     return {
-        name,
-        notes,
-        created,
-        due,
-        completed,
-        priority,
+        editName,
+        editNotes,
+        editDue,
+        editPriority,
+        getName,
+        getDue,
+        getPriority,
+        getID,
     }
 }
 
