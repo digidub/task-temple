2a184af8d5b745b96c66a7c0b9905591e91be83f
Sholto <madein.mania@gmail.com>
2021-04-07 10:37:20 +0100

    removed unnecessary init code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import './style.css';
+import Fb from './firebase'
 import { Project } from './projects'
 import { Task } from './tasks'
-import { appControl } from './appcontrol'
 import { lsControl } from './localstorage'
-
+import { appControl } from './appcontrol'
+import addIcon from './add.svg'
+import { DOMcontrol } from './domcontrol'
+import { appData } from './appdata';
