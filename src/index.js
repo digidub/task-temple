52355c1e0aed29eafee5bdb3f0efbd997b1701de
Sholto <madein.mania@gmail.com>
2021-04-12 09:43:48 +0100

    img loaders moved to domcontrol

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Project } from './projects'
 import { Task } from './tasks'
 import { lsControl } from './localstorage'
 import { appControl } from './appcontrol'
-import addIcon from './add.svg'
 import { DOMcontrol } from './domcontrol'
 import { appData } from './appdata';
 
