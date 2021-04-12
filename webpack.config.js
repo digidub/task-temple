a49654d7c8d225915e4ceec02ea11ece45f90ac6
Sholto <madein.mania@gmail.com>
2021-04-12 08:57:19 +0100

    webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,41 @@ const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: './src/index.js',
     devtool: 'inline-source-map',
     module: {
         rules: [
             {
+                test: /\.js$/,
+                enforce: "pre",
+                use: ["source-map-loader"],
+            },
+            {
                 test: /\.css$/,
                 use: ["style-loader", "css-loader"]
-            }
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
         ]
     },
     devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
+        contentBase: './src',
         port: 9000,
     },
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
     plugins: [
         new HtmlWebpackPlugin({
+            filename: "index.html",
             template: path.resolve(__dirname, "src", "index.html"),
-            title: "development"
-        })
+            title: "Development",
+            inject: false,
+        }),
     ]
 };
