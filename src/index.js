d0acc332222f2fce553da81037b651d2e0d52c34
Sholto <madein.mania@gmail.com>
2021-04-12 15:12:59 +0100

    removed unused import

--- src/index.js
+++ src/index.js
@@ -6,5 +6,4 @@ import { lsControl } from './localstorage'
 import { appControl } from './appcontrol'
 import { DOMcontrol } from './domcontrol'
 import { appData } from './appdata';
-import addIcon from './add.svg'
 import favicon from './favicon.ico'
